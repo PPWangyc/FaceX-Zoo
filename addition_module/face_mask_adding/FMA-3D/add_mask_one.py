3f4f23cf45abfa4ca4a98a4cbc7982b6576ec8e4
PPWang <ywang330@u.rochester.edu>
2023-02-14 17:23:24 -0500

    add

--- addition_module/face_mask_adding/FMA-3D/add_mask_one.py
+++ addition_module/face_mask_adding/FMA-3D/add_mask_one.py
@@ -4,14 +4,25 @@
 @contact: jun21wangustc@gmail.com
 """
 
+# add arguments
+import argparse
+
+argparser = argparse.ArgumentParser(description='Face Masking')
+argparser.add_argument('--image_path', type=str, default='Data/test-data/test1.jpg', help='path to input image')
+argparser.add_argument('--face_lms_file', type=str, default='Data/test-data/test1_landmark.txt', help='path to input image')
+argparser.add_argument('--template_name', type=str, default='7.png', help='path to input image')
+argparser.add_argument('--masked_face_path', type=str, default='test1_mask1.jpg', help='path to input image')
+
+args = argparser.parse_args()
+
 from face_masker import FaceMasker
 
 if __name__ == '__main__':
     is_aug = False
-    image_path = 'Data/test-data/test1.jpg'
-    face_lms_file = 'Data/test-data/test1_landmark.txt'
-    template_name = '7.png'
-    masked_face_path = 'test1_mask1.jpg'
+    image_path = args.image_path
+    face_lms_file = args.face_lms_file
+    template_name = args.template_name
+    masked_face_path = args.masked_face_path
     face_lms_str = open(face_lms_file).readline().strip().split(' ')
     face_lms = [float(num) for num in face_lms_str]
     face_masker = FaceMasker(is_aug)
--- addition_module/face_mask_adding/FMA-3D/process_data.py
+++ addition_module/face_mask_adding/FMA-3D/process_data.py
@@ -1,4 +1,5 @@
 import os
+import random
 import subprocess
 
 # get current folder path
@@ -8,6 +9,7 @@ print(current_path)
 data_storage_path= os.path.join(current_path, 'Data/data')
 data_landmark_path=os.path.join(current_path, 'Data/data_landmark')
 data_detect_path=os.path.join(current_path, 'Data/data_detect')
+data_mask_path=os.path.join(current_path, 'Data/data_mask')
 
 
 
@@ -26,6 +28,8 @@ print(os.listdir())
 
 # for each .jpg file, run the command to get the landmark
 for file in file_list:
+    # generate a random number from 0 - 7, include 0 and 7
+    template_name = str(random.randint(0, 7)) + '.png'
     file_name = file.split('.')[0]
     command = 'python api_usage/face_detect.py --image_path ' + data_storage_path + '/' + file + ' --output_path_txt ' + data_detect_path + '/' + file_name + '_detect.txt'
     print(command)
@@ -33,4 +37,9 @@ for file in file_list:
     command = 'python api_usage/face_alignment.py --image_path ' + data_storage_path + '/' + file + ' --image_det_txt_path ' + data_detect_path + '/' + file_name + '_detect.txt' + ' --output_path_txt ' + data_landmark_path + '/' + file_name + '_landmark.txt'
     print(command)
     subprocess.call(command, shell=True)
-    command = 'python api_usage/face_alignment.py --image_p'
+    command = 'python api_usage/face_alignment.py --image_path ' + data_storage_path + '/' + file + ' --face_lms_file' + data_landmark_path + '/' + file_name + '_landmark0.txt' + ' --masked_face_path ' + data_mask_path + '/' + file_name + '_mask.jpg' + ' --template_name ' + template_name
+    print(command)
+    subprocess.call(command, shell=True)
+
+# cd back to the original folder
+os.chdir(current_path)
